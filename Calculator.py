09be78dfcf659d56291af1fbcab857e0999a55b4
Tim W <142520824+timmyd4@users.noreply.github.com>
2024-10-14 16:17:41 -0700

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -10,10 +10,7 @@ def add(x, y):
     return x + y
 
 def subtract(x, y):
-    if x > y:
-        return y - x
-    else:
-        return x - y
+    return x - y
     
 def multiply(x, y):
     return x * y
